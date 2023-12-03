23b40e5f9400e9f593e79810b13bf21e7a6acda4
Ross MacLeod <rmm+github@z.odi.ac>
2023-12-03 04:02:03 +0000

    day02a

---
+++ src/day02a/Main.hs
@@ -0,0 +1,86 @@
+module Main where
+
+import Control.Lens (_1, _3, folded, view, sumOf, each, filtered)
+import Control.Lens.TH (makeClassy)
+import Data.Attoparsec.Combinator (sepBy)
+import Data.Attoparsec.Text.Lazy
+  ( Parser,
+    char,
+    decimal,
+    endOfInput,
+    endOfLine,
+    parseOnly,
+    skipSpace,
+    string,
+  )
+import Data.Text.Lazy.IO (getContents)
+import Data.Typeable (typeRep)
+import Text.Show qualified as TS
+
+data Observation = Observation {_red :: Int, _green :: Int, _blue :: Int}
+  deriving stock (Eq)
+
+pattern Red, Green, Blue :: Int -> Observation
+pattern Red r <- Observation r _ _ where Red r = Observation r 0 0
+pattern Green g <- Observation _ g _ where Green r = Observation 0 r 0
+pattern Blue b <- Observation _ _ b where Blue r = Observation 0 0 r
+
+instance Ord Observation where
+  Observation ra ga ba <= Observation rb gb bb = ra <= rb && ga <= gb && ba <= bb
+
+instance Show Observation where
+  show (Observation r g b) = intercalate ", " . catMaybes $ [f "red" r, f "green" g, f "blue" b]
+    where
+      f _ 0 = Nothing
+      f s n = Just $ show n <> " " <> s
+
+instance Semigroup Observation where
+  Observation ra ga ba <> Observation rb gb bb =
+    Observation (ra `max` rb) (ga `max` gb) (ba `max` bb)
+
+instance Monoid Observation where
+  mempty = Observation 0 0 0
+
+data Game = Game
+  { _gameNo :: Int,
+    _observations :: NonEmpty Observation
+  }
+  deriving stock (Eq)
+
+instance Show Game where
+  show (Game n obs) = "Game " <> show n <> ": " <> (intercalate "; " . fmap show . toList $ obs)
+
+makeClassy ''Game
+
+colorP :: Parser (Int -> Observation)
+colorP = Red <$ string "red" <|> Green <$ string "green" <|> Blue <$ string "blue"
+
+qtyColorP :: Parser Observation
+qtyColorP = flip ($) <$> decimal <*> (skipSpace *> colorP)
+
+nonEmptyP :: forall a. Typeable a => Parser [a] -> Parser (NonEmpty a)
+nonEmptyP = (=<<) $ maybe (fail $ "empty list of " <> show (typeRep (Proxy @a))) pure . nonEmpty
+
+observationP :: Parser Observation
+observationP = fmap sconcat . nonEmptyP $ (skipSpace *> qtyColorP) `sepBy` char ','
+
+observationsP :: Parser (NonEmpty Observation)
+observationsP = nonEmptyP $ (skipSpace *> observationP) `sepBy` char ';'
+
+gameP :: Parser Game
+gameP = Game <$> (string "Game" *> skipSpace *> decimal) <*> (char ':' *> observationsP)
+
+main :: IO ()
+main = do
+  let limit = Observation 12 13 14
+  games <-
+    either (fail . show) (pure . catMaybes)
+      . parseOnly ((optional gameP `sepBy` endOfLine) <* endOfInput)
+      =<< getContents
+  let summedGames =
+        games <&> \g ->
+          let obsum = view (observations . folded) g
+           in (g, obsum, obsum <= limit)
+  for_ summedGames $ \(g, obsum, possible) ->
+    putStrLn $ bool "" "Possible " possible <> show g <> " => " <> show obsum
+  print $ sumOf (each . filtered (view _3) . _1 . gameNo) summedGames
---
+++ src/day02a/main.rs
@@ -0,0 +1,196 @@
+use nom::branch::alt;
+use nom::bytes::complete::tag;
+use nom::character::complete::{space0, space1, u32};
+use nom::combinator::map;
+use nom::multi::separated_list1;
+use nom::sequence::preceded;
+use nom::IResult;
+use std::cmp::Ordering;
+
+#[derive(Debug, PartialEq)]
+struct Observation {
+    red: u32,
+    green: u32,
+    blue: u32,
+}
+
+impl PartialOrd<Observation> for Observation {
+    fn partial_cmp(&self, rhs: &Observation) -> Option<Ordering> {
+        if self.red == rhs.red && self.green == rhs.green && self.blue == rhs.blue {
+            Some(Ordering::Equal)
+        } else if self.red <= rhs.red && self.green <= rhs.green && self.blue <= rhs.blue {
+            Some(Ordering::Less)
+        } else {
+            Some(Ordering::Greater)
+        }
+    }
+}
+
+impl Default for Observation {
+    fn default() -> Self {
+        Self {
+            red: 0,
+            green: 0,
+            blue: 0,
+        }
+    }
+}
+
+impl std::ops::Add<&Observation> for &Observation {
+    type Output = Observation;
+    fn add(self, rhs: &Observation) -> Observation {
+        Observation {
+            red: self.red.max(rhs.red),
+            green: self.green.max(rhs.green),
+            blue: self.blue.max(rhs.blue),
+        }
+    }
+}
+
+impl std::ops::AddAssign<&Observation> for Observation {
+    fn add_assign(&mut self, rhs: &Observation) {
+        self.red = self.red.max(rhs.red);
+        self.green = self.green.max(rhs.green);
+        self.blue = self.blue.max(rhs.blue);
+    }
+}
+
+impl<'a> std::iter::Sum<&'a Observation> for Observation {
+    fn sum<I: Iterator<Item = &'a Self>>(iter: I) -> Self {
+        iter.fold(Observation::default(), |mut obsum, ob| {
+            obsum += ob;
+            obsum
+        })
+    }
+}
+
+impl std::fmt::Display for Observation {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        let mut first = true;
+        let mut w = |n, s| {
+            if n <= 0 {
+                return Ok(());
+            }
+
+            if first {
+                first = false;
+            } else {
+                write!(f, ", ")?;
+            }
+
+            write!(f, "{} {}", n, s)
+        };
+        w(self.red, "red")?;
+        w(self.green, "green")?;
+        w(self.blue, "blue")?;
+        Ok(())
+    }
+}
+
+#[derive(Debug)]
+struct Game {
+    game_no: u32,
+    observations: Vec<Observation>,
+}
+
+impl std::fmt::Display for Game {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "Game {}: ", self.game_no)?;
+        let mut first = true;
+        for ob in self.observations.iter() {
+            if first {
+                first = false;
+            } else {
+                write!(f, "; ")?;
+            }
+            write!(f, "{}", ob)?;
+        }
+        Ok(())
+    }
+}
+
+fn main() {
+    let limit = Observation {
+        red: 12,
+        green: 13,
+        blue: 14,
+    };
+    let games = std::io::stdin().lines().map(|l| {
+        let l = l.unwrap();
+        match parse_game(&*l) {
+            Ok(("", n)) => n,
+            other => panic!("canne parse {l:?}: {other:x?}"),
+        }
+    });
+    let games_summed: Vec<(Game, Observation, bool)> = games
+        .into_iter()
+        .map(|g| {
+            let obsum: Observation = g.observations.iter().sum();
+            let possible = &obsum <= &limit;
+            (g, obsum, possible)
+        })
+        .collect();
+
+    for (g, obsum, possible) in games_summed.iter() {
+        println!(
+            "{}{} => {}",
+            if *possible { "Possible " } else { "" },
+            g,
+            obsum
+        );
+    }
+
+    println!(
+        "{}",
+        games_summed
+            .iter()
+            .filter(|(_, _, possible)| *possible)
+            .map(|(g, _, _)| g.game_no)
+            .sum::<u32>()
+    );
+}
+
+fn parse_game(inp: &str) -> IResult<&str, Game> {
+    let (inp, _) = tag("Game")(inp)?;
+    let (inp, _) = space1(inp)?;
+    let (inp, game_no) = u32(inp)?;
+    let (inp, _) = tag(":")(inp)?;
+    let (inp, observations) = parse_observations(inp)?;
+    Ok((
+        inp,
+        Game {
+            game_no,
+            observations,
+        },
+    ))
+}
+
+fn parse_observations(inp: &str) -> IResult<&str, Vec<Observation>> {
+    separated_list1(tag(";"), preceded(space0, parse_observation))(inp)
+}
+
+fn parse_observation(inp: &str) -> IResult<&str, Observation> {
+    map(
+        separated_list1(tag(","), preceded(space0, parse_qty_color)),
+        |obs| obs.iter().sum(),
+    )(inp)
+}
+
+fn parse_qty_color(inp: &str) -> IResult<&str, Observation> {
+    let (inp, qty) = nom::character::complete::u32(inp)?;
+    let (inp, _) = space1(inp)?;
+    alt((
+        map(tag("red"), move |_| Observation {
+            red: qty,
+            ..Observation::default()
+        }),
+        map(tag("green"), move |_| Observation {
+            green: qty,
+            ..Observation::default()
+        }),
+        map(tag("blue"), move |_| Observation {
+            blue: qty,
+            ..Observation::default()
+        }),
+    ))(inp)
+}
